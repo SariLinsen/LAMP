6f406628c2cf6789a56a09e6a2bce0502e50dc2f
Sari Linsén <sari.linsen@hotmail.fi>
2022-12-08 00:04:03 +0000

    update

--- script.sh
+++ script.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-echo "Päivitetään pakettilistaus ajantasalle.."
+echo "Päivitetään pakettilistaus ajantasalle, asennetaan git ja salt.."
 sudo apt-get update
-sudo apt-get -y install git
+sudo apt-get -y install git salt-minion
 
 echo "Luodaan kansio kotisivujasi varten.."
 mkdir public_html
 
 echo "Haetaan tiedostot Githubista.."
 git clone https://github.com/SariLinsen/LAMP.git
+cd LAMP/
+
+echo "Luodaan kansio /srv/salt/ ja kopioidaan tiedostot.."
+sudo mkdir /srv/salt/
+sudo cd -R apache /srv/salt/
+
+sudo salt-call --local state.apply apache
+
